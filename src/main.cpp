a237e4c525e2e532cf5f07ed39c456b6e0599971
Matej <smycka@ics.muni.cz>
2024-08-31 13:41:41 +0200

    init

---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+#include <Arduino.h>
+#include <DEV_Config.h>
+// tft display
+#include <SPI.h>
+#include <LCD_Driver.h>
+
+void setup()
+{
+  Serial.begin(9600);
+  Config_Init();
+  LCD_Init();
+  LCD_SetBacklight(1000);
+}
+
+void loop()
+{
+  Serial.println("Hello, World!");
+  delay(1000);
+}
